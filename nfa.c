0d3834955b28fca381c67f98f01cd6ebde298656
gwg <ggundersen@gmail.com>
2015-01-10 11:54:31 -0500

    Defined new_list() helper function

--- nfa.c
+++ nfa.c
@@ -1,6 +1,12 @@
 #include "nfa.h"
 
 
+/* Use 256 and 257 for Match and Split to stay outside of ASCII's range. */
+enum {
+	Match = 256,
+	Split = 257
+};
+
 /* We represent the NFA as a linked collection of State structs. */
 struct State
 {
@@ -10,7 +16,10 @@ struct State
     int lastlist;
 };
 
-/* I'm not sure why RSC uses a union: http://stackoverflow.com/a/346579/1830334. */
+/* 
+ * I'm not sure why RSC uses a union. See:
+ * http://stackoverflow.com/a/346579/1830334.
+ */
 union Ptrlist
 {
     Ptrlist *next;
@@ -26,3 +35,19 @@ struct Frag
 	struct State *start;
 	Ptrlist *out;
 };
+
+/* Creates a new pointer list containing a single pointer outp. */
+Ptrlist *new_ptrlist(State **outp)
+{
+    /*
+     * Called "type punning": http://en.wikipedia.org/wiki/Type_punning.
+     * "This works because C does not care about the underlying data in an
+     * address, just what you represent it as."
+     */
+    Ptrlist *nlist = (Ptrlist*) outp;
+    nlist->next = NULL;
+    return nlist;
+};
+
+//Ptrlist *append(Ptrlist *l1, Ptrlist *l2);
+//void patch(Ptrlist *l, State *s);
--- nfa.h
+++ nfa.h
@@ -2,13 +2,20 @@
 #define NFA_H
 
 
-/* These typedefs allows us to reference `X` rather than `Struct X`. */
+#define NULL 0
+
+
+/* These typedefs allows us to reference "X" rather than "Struct X". */
 
 typedef struct State State;
 /* I'm not sure why RSC uses a union: http://stackoverflow.com/a/346579/1830334. */
 typedef union Ptrlist Ptrlist;
 typedef struct Frag Frag;
 
+/* Helper functions for manipulating NFA states. */
+Ptrlist *new_ptrlist(State **outp);
+//Ptrlist *append(Ptrlist *l1, Ptrlist *l2);
+//void patch(Ptrlist *l, State *s);
 
 
 #endif
