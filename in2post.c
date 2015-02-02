09a572f4df550aabb22418aa6976b5606f270f62
gwg <ggundersen@gmail.com>
2015-02-02 08:35:46 -0500

    Hacky version of decrementing stack pointer

--- in2post.c
+++ in2post.c
@@ -10,7 +10,7 @@
  * symbol conflicts with the other pop and push functions. See:
  * http://stackoverflow.com/a/572550/1830334.
  */
-/*static char pop(char **stackp)
+static char pop(char **stackp)
 {
     (*stackp)--;
     return **stackp;
@@ -20,30 +20,37 @@ static void push(char **stackp, char c)
 {
     **stackp = c;
     (*stackp)++;
-}*/
+}
 
 /* Details here: http://csis.pace.edu/~wolf/CS122/infix-postfix.htm. */
 char *in2post(char *infix)
 {
-    char *p, /*op_stack[1000], *stackp,*/ buff[8000], *post;
+    char *p, op_stack[1000], *stackp, buff[8000], *post;
 
     post = buff;
-    //stackp = op_stack;
+    stackp = op_stack;
+    *stackp = '~';
+    stackp++;
     for (p = infix; *p != '\0'; p++) {
 		switch (*p) {
             default:
-                printf("%c\n", *p);
                 *post = *p;
                 post++;
-                //push(&stackp, *p);
                 break;
 
-            /*case '+':
-                break;*/
+            case '+':
+                push(&stackp, *p);
+                break;
         }
     }
 
-    *post = 0;
+    /* Pop all remaining operators off of the stack. */
+    while (*stackp != '~') {
+        char c = pop(&stackp);
+        printf("%c\n", c);
+        *post = c; 
+        post++;
+    }
 
     printf("Final: %s\n", buff);
     return post;
